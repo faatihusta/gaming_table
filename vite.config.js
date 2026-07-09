c1e4be7f7b7402cf3181c424c725a63ace06ce13
Fatih Usta <faatihusta@gmail.com>
2026-07-10 00:07:18 +0300

    Build docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/gaming_table/'
+  base: '/gaming_table/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
